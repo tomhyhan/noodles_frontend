c9d1db888739855cd5dcb8711974dbde84220948
Tom Han <tomhyhan@gmail.com>
2025-01-09 23:14:38 -0800

    fixed mobile view

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,12 +28,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang='en'>
-      <head>
-        <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1.0, maximum-scale=1.0'
-        />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen min-h-screen bg-amber-50 relative overflow-x-hidden flex flex-col justify-center`}
       >
